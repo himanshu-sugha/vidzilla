ac79aae800eccb93d0f743c166677963bad7095d
himanshu-sugha <himanshusugha@gamil.com>
2024-08-04 19:28:36 +0530

    final app completed

--- src/components/Utils/constants.js
+++ src/components/Utils/constants.js
@@ -1,6 +1,25 @@
  const GOOGLE_API_KEY="AIzaSyCvFmxeLHGXeZ285886WEuHev9W8QJ81eA"
 
+ export const VIDEO_API =
+ "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=" +
+ GOOGLE_API_KEY +
+ "&id=";
 
-export const YOUTUBE_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  GOOGLE_API_KEY;
+ export const YOUTUBE_API =
+ "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+ GOOGLE_API_KEY;
+ 
+  
+  export const YOUTUBE_SEARCH_API =
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&key=" +
+  GOOGLE_API_KEY +
+  "&q=";
+
+
+  export const COMMENTS_API =
+  "https://www.googleapis.com/youtube/v3/commentThreads?textFormat=plainText&part=snippet&maxResults=50&key=" +
+  GOOGLE_API_KEY +
+  "&videoId=";
+
+
+
--- src/components/Utils/store.js
+++ src/components/Utils/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import appSlice from './appSlice'
+import liveChatSlice from './liveChatSlice';
 
 const store = configureStore({
     reducer:{
         app:appSlice,
+        chat: liveChatSlice,
     }
 }
 );
